61bee83ac94eaea730748db68c176b42d32f6914
Michael Blydenburgh <mblydenburgh@icloud.com>
2018-11-29 14:50:35 +0000

    server.js change

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const liri = require("liri");
 var port = process.env.PORT || 3000;
 var app = express();
 app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+ res.send(liri);
 });
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
