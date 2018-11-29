cc44bc9697e684a0f02533c57eb8618ec11d0c21
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-11-28 20:04:49 -0500

    Reprompt after data return, add quit action

--- README.md
+++ README.md
@@ -11,8 +11,9 @@
 Scroll through the options for LIRI with the arrow keys, select items with the Enter key
 
 #### Finding Concerts
-Selecting the "Find Concert" action from the initial menu will prompt for an Artist name to search for. This dynamically generates a search url that will be called to with Axios. The data returned to the screen is the venue name, venue location, and event date.
+Selecting the "Find Concert" action from the initial menu will prompt the user for an Artist name to search for. This dynamically generates a search url that will be called to with Axios. The data returned to the screen is the venue name, venue location, and event date.
 
 #### Finding Songs
+Selecting the "Spotify A Song" action prompts the user for a track name to query the Spotify API via the Node Spotify API. A Maximum of 20 results will be returned for matches. Response data includes Album Name, Artist, Release Year, and Spotify URL.
 
 #### Finding Movies
--- liri.js
+++ liri.js
@@ -27,28 +27,28 @@ const bandsInTownPrompt = function () {
         });
 };
 
-const bandsInTown = function(query){
+const bandsInTown = function (query) {
     artistUrl = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
-            console.log(artistUrl);
-            axios.get(artistUrl)
-                .then(function (response) {
-                    let { data: artistData } = response;
-                    //console.log(artistData); //array of objects detailing concert events
-                    artistEvents = artistData.map((event, index) => {
-                        let { venue: venueData, datetime: eventTime } = event;
-                        let dateObj = new Date(eventTime);
-                        let momentObj = moment(dateObj);
-                        let formattedDate = momentObj.format(`MMM Do YYYY`);
-                        console.log(`******Event ${index+1} Information******`);
-                        console.log(`Venue Name: ${venueData.name}`);
-                        console.log(`Location: ${venueData.city}, ${venueData.region}`);
-                        console.log(`Date: ${formattedDate}`);
-                    });
-
-                })
-                .catch(function(error){
-                    console.log(error);
-                });
+    console.log(artistUrl);
+    axios.get(artistUrl)
+        .then(function (response) {
+            let { data: artistData } = response;
+            //console.log(artistData); //array of objects detailing concert events
+            artistEvents = artistData.map((event, index) => {
+                let { venue: venueData, datetime: eventTime } = event;
+                let dateObj = new Date(eventTime);
+                let momentObj = moment(dateObj);
+                let formattedDate = momentObj.format(`MMM Do YYYY`);
+                console.log(`******Event ${index + 1} Information******`);
+                console.log(`Venue Name: ${venueData.name}`);
+                console.log(`Location: ${venueData.city}, ${venueData.region}`);
+                console.log(`Date: ${formattedDate}`);
+            });
+            actionPrompt();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
 const spotifyASongPrompt = function () {
@@ -66,150 +66,156 @@ const spotifyASongPrompt = function () {
 
 };
 
-const spotifyASong = function(query){
+const spotifyASong = function (query) {
     console.log(`searching for ${query}`);
-            spotify.search({ type: "track", query: query })
-                .then(data => { 
-                    //console.log(data);
-                    const {items} = data.tracks;
-                    items.map((item,index)=>{
-                       //console.log(item);
-                       const {preview_url:url,name:trackName} = item;
-                       const {name:artistName}=item.artists[0];
-                       const {name:albumName,release_date:date}=item.album
-                       
-                       console.log(`-----Result ${index+1}-----`);
-                       console.log(`Track Name: ${trackName}`);
-                       console.log(`Album: ${albumName}`)
-                       console.log(`Artist: ${artistName}`);
-                       console.log(`Release Date: ${date}`);
-                       console.log(`Spotify URL: ${url}`);
-                       
-                    });
-                    
-                })
-                .catch(error => { console.log(error) });
+    spotify.search({ type: "track", query: query })
+        .then(data => {
+            //console.log(data);
+            const { items } = data.tracks;
+            items.map((item, index) => {
+                //console.log(item);
+                const { preview_url: url, name: trackName } = item;
+                const { name: artistName } = item.artists[0];
+                const { name: albumName, release_date: date } = item.album
+
+                console.log(`-----Result ${index + 1}-----`);
+                console.log(`Track Name: ${trackName}`);
+                console.log(`Album: ${albumName}`)
+                console.log(`Artist: ${artistName}`);
+                console.log(`Release Date: ${date}`);
+                console.log(`Spotify URL: ${url}`);
+
+            });
+        actionPrompt();
+        })
+        .catch(error => { console.log(error) });
 };
 
-const searchOMDBPrompt = function(){
+const searchOMDBPrompt = function () {
     inquirer.prompt([
         {
-            type:"input",
-            name:"movie",
-            message:"Enter a movie title to search OMDB for"
+            type: "input",
+            name: "movie",
+            message: "Enter a movie title to search OMDB for"
         }
     ])
-    .then(function(response){
-        let query = response.movie;
-        searchOMDB(query);
-    });
-    
+        .then(function (response) {
+            let query = response.movie;
+            searchOMDB(query);
+        });
+
 };
 
-const searchOMDB = function(query){
+const searchOMDB = function (query) {
+    moviesArray = [];
+    sortedMovies = [];
+    imdbIds = [];
     console.log(`searching for ${query}`);
-        movieUrl = `http://www.omdbapi.com/?apikey=trilogy&s=${query}`;
-        axios.get(movieUrl)
-        .then(function(response){
+    movieUrl = `http://www.omdbapi.com/?apikey=trilogy&s=${query}`;
+    axios.get(movieUrl)
+        .then(function (response) {
             //console.log(response.data.Search);
             let movieResults = response.data.Search;
-            movieResults.map(movie=>{
+            movieResults.map(movie => {
                 imdbIds.push(movie.imdbID);
             });
             //console.log(imdbIds);
-            imdbIds.map((imdbId,index)=>{
+            imdbIds.map((imdbId, index) => {
                 movieUrl = `http://www.omdbapi.com/?apikey=trilogy&i=${imdbId}`;
                 axios.get(movieUrl)
-                .then(function(response){
-                    //console.log(response.data);
-                    let {Title, Year, imdbRating, Country, Language, Plot, Actors, Ratings } = response.data;
-                    console.log(`------Loading Movie Response ${index+1}------`);
-                    // console.log(Title);
-                    // console.log(Year);
-                    // console.log(imdbRating);
-                    // console.log(Country);
-                    // console.log(Language);
-                    // console.log(Plot);
-                    // console.log(Actors);
-                    //console.log(Ratings[1]);
+                    .then(function (response) {
+                        //console.log(response.data);
+                        let { Title, Year, imdbRating, Country, Language, Plot, Actors, Ratings } = response.data;
+                        console.log(`------Loading Movie Response ${index + 1}------`);
+                        // console.log(Title);
+                        // console.log(Year);
+                        // console.log(imdbRating);
+                        // console.log(Country);
+                        // console.log(Language);
+                        // console.log(Plot);
+                        // console.log(Actors);
+                        //console.log(Ratings[1]);
 
 
-                    //since some movies do not have a "Ratings" key, push empty data so newMovie can be made without errors
-                    if(Ratings[0] === undefined){
-                        Ratings.push({Value:"N/A"},{Value:"N/A"});
-                    }
+                        //since some movies do not have a "Ratings" key, push empty data so newMovie can be made without errors
+                        if (Ratings[0] === undefined) {
+                            Ratings.push({ Value: "N/A" }, { Value: "N/A" });
+                        }
 
-                    if(Ratings[1] === undefined){
-                        Ratings.push({Value:"N/A"});
-                    }
-                    
-                    //console.log(Ratings)
+                        if (Ratings[1] === undefined) {
+                            Ratings.push({ Value: "N/A" });
+                        }
 
-                    let newMovie = {
-                        id: index,
-                        title: Title,
-                        year: Year,
-                        imdbRating: imdbRating,
-                        rottenTomatoScore: Ratings[1].Value,
-                        country: Country,
-                        language: Language,
-                        plot: Plot,
-                        actors: Actors
-                    };
-                    moviesArray.push(newMovie);
+                        //console.log(Ratings)
 
-                    //once the last api call is returned, print the data
-                    if(moviesArray.length === imdbIds.length){
-                        //console.log(moviesArray);
-                        let sortedMovies = moviesArray.sort(function(a,b){
-                            return a.id - b.id;
-                        });
-                        //console.log(sortedMovies);
+                        let newMovie = {
+                            id: index,
+                            title: Title,
+                            year: Year,
+                            imdbRating: imdbRating,
+                            rottenTomatoScore: Ratings[1].Value,
+                            country: Country,
+                            language: Language,
+                            plot: Plot,
+                            actors: Actors
+                        };
+                        moviesArray.push(newMovie);
 
-                        sortedMovies.map(movie=>{
-                            console.log(`******Result ${movie.id+1}******`);
-                            console.log(`Title: ${movie.title}`);
-                            console.log(`Released: ${movie.year}`);
-                            console.log(`IMDB Rating: ${movie.imdbRating}`);
-                            console.log(`Rotten Tomato Score: ${movie.rottenTomatoScore}`);
-                            console.log(`Country of Origin: ${movie.country}`);
-                            console.log(`Language: ${movie.language}`);
-                            console.log(`Plot: ${movie.plot}`);
-                            console.log(`Actors: ${movie.actors}`);
-                        });
-                    }
-                })
-                .catch(function(error){
-                    console.log(error);
-                });
+                        //once the last api call is returned, print the data
+                        if (moviesArray.length === imdbIds.length) {
+                            //console.log(moviesArray);
+                            let sortedMovies = moviesArray.sort(function (a, b) {
+                                return a.id - b.id;
+                            });
+                            //console.log(sortedMovies);
+
+                            sortedMovies.map(movie => {
+                                console.log(`******Result ${movie.id + 1}******`);
+                                console.log(`Title: ${movie.title}`);
+                                console.log(`Released: ${movie.year}`);
+                                console.log(`IMDB Rating: ${movie.imdbRating}`);
+                                console.log(`Rotten Tomato Score: ${movie.rottenTomatoScore}`);
+                                console.log(`Country of Origin: ${movie.country}`);
+                                console.log(`Language: ${movie.language}`);
+                                console.log(`Plot: ${movie.plot}`);
+                                console.log(`Actors: ${movie.actors}`);
+                                
+                            });
+                            actionPrompt();
+                        }
+                        
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
             });
         })
-        .catch(function(error){
+        .catch(function (error) {
             console.log(error);
         });
 };
 
-const doRandom = function(){
-    fs.readFile('random.txt', 'utf-8',function(error,data){
-       if(error){
-           console.log(error);
-       }
-       console.log(data);
-       const randomActions = data.split(",");
-       console.log(randomActions);
-       
-       randomIndex = Math.floor(Math.random()*(randomActions.length-1));
-       if(randomIndex % 2 !== 0){
-           randomIndex -= 1;
-       }
-       console.log(randomIndex);
-       console.log(randomActions[randomIndex]);
-       console.log(randomActions[randomIndex+1]);
-       let randomAction = randomActions[randomIndex];
-       let randomParameter = randomActions[randomIndex+1];
-       
-       switch (randomAction){
-           case "Find Concert":
+const doRandom = function () {
+    fs.readFile('random.txt', 'utf-8', function (error, data) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(data);
+        const randomActions = data.split(",");
+        console.log(randomActions);
+
+        randomIndex = Math.floor(Math.random() * (randomActions.length - 1));
+        if (randomIndex % 2 !== 0) {
+            randomIndex -= 1;
+        }
+        console.log(randomIndex);
+        console.log(randomActions[randomIndex]);
+        console.log(randomActions[randomIndex + 1]);
+        let randomAction = randomActions[randomIndex];
+        let randomParameter = randomActions[randomIndex + 1];
+
+        switch (randomAction) {
+            case "Find Concert":
                 console.log(`Finding concert...`);
                 bandsInTown(randomParameter);
                 break;
@@ -221,37 +227,45 @@ const doRandom = function(){
                 console.log(`Finding movie...`);
                 searchOMDB(randomParameter);
                 break;
-       }
-       
+        }
+
     });
 };
 
 
-//Initial prompt on application start to choose an action for LIRI
-inquirer.prompt([
-    {
-        type: "list",
-        name: "action",
-        choices: ["Find Concert", "Spotify A Song", "Find Movie", "I'm Feeling Lucky..."]
-    },
-])
-    .then(function (response) {
-        switch (response.action) {
-            case "Find Concert":
-                console.log(`Finding concert...`);
-                bandsInTownPrompt();
-                break;
-            case "Spotify A Song":
-                console.log(`Finding song...`);
-                spotifyASongPrompt();
-                break;
-            case "Find Movie":
-                console.log(`Finding movie...`);
-                searchOMDBPrompt();
-                break;
-            case "I'm Feeling Lucky...":
-                console.log(`Doing what it says...`);
-                doRandom();
-                break;
-        }
-    })
+//Prompt user to select an action for LIRI
+function actionPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            choices: ["Find Concert", "Spotify A Song", "Find Movie", "I'm Feeling Lucky...","Quit"]
+        },
+    ])
+        .then(function (response) {
+            switch (response.action) {
+                case "Find Concert":
+                    console.log(`Finding concert...`);
+                    bandsInTownPrompt();
+                    break;
+                case "Spotify A Song":
+                    console.log(`Finding song...`);
+                    spotifyASongPrompt();
+                    break;
+                case "Find Movie":
+                    console.log(`Finding movie...`);
+                    searchOMDBPrompt();
+                    break;
+                case "I'm Feeling Lucky...":
+                    console.log(`Doing what it says...`);
+                    doRandom();
+                    break;
+                case "Quit":
+                    console.log(`Buuh Bye Now!`);
+                    break;
+            }
+        })
+}
+
+//Start LIRI by initializing action prompt
+actionPrompt();
